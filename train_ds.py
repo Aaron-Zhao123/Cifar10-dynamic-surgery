7ecbba93d1164475057884e5d41d30276b15848b
Aaron Zhao <aaron@user-32-56.vpn.net.private.cam.ac.uk>
2017-03-18 00:05:45 +0800

    changed run files

--- train_ds.py
+++ train_ds.py
@@ -51,12 +51,12 @@ def _variable_on_cpu(name, shape, initializer):
 
 def dynamic_surgery(weight, pruning_th, recover_percent):
     threshold = np.percentile(np.abs(weight),pruning_th)
-    soft_threshold = np.percentile(np.abs(weight),recover_percent*pruning_th)
+    soft_threshold = np.percentile(np.abs(weight),(recover_percent*pruning_th))
     weight_mask = np.abs(weight) > threshold
     soft_weight_mask = (np.abs(weight) > soft_threshold) - weight_mask
 
     print(79*'*')
-    print('recover_rate {}'.format(recover_percent))
+    print('pruning th {} recover_rate {}'.format(pruning_th, recover_percent))
     print('{},{}'.format(threshold, soft_threshold))
     # print('test mask {}'.format(np.sum(weight_mask)))
     return (weight_mask, soft_weight_mask)
