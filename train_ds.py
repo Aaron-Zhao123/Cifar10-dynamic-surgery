1467f323d07f6a298f7fd731554991650fafbd37
Aaron Zhao <aaron@user-32-56.vpn.net.private.cam.ac.uk>
2017-03-17 15:55:24 +0800

    changed run files

--- train_ds.py
+++ train_ds.py
@@ -65,7 +65,7 @@ def recover_weights(weights_mask, biases_mask, soft_weight_mask, soft_biase_mask
         biases_mask[key] = biases_mask[key] + (soft_biase_mask[key] * np.random.rand(*soft_biase_mask[key].shape) > 0.5)
     print("test in recover weights")
     print(np.array_equal(prev, weights_mask['fc1']))
-    sys.exit()
+    # sys.exit()
     mask_info(weights_mask)
     return (weights_mask, biases_mask)
 
